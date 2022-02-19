8b0b5365f53c52b8947ebb9922746024dce9155c
Guanchen Zhao <guanchenzhao@gmail.com>
2022-02-18 18:08:36 -0800

    Create Permutations.java

---
+++ src/leetcode/solution/BackTracking/Permutations.java
@@ -0,0 +1,58 @@
+package leetcode.solution.BackTracking;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 46. Permutations
+ * Given an array nums of distinct integers, return all the possible permutations.
+ * You can return the answer in any order.
+ */
+public class Permutations {
+
+    public static void main(String[] args) {
+        int[] candidates = new int[]{1, 2, 3};
+
+        System.out.println(permute(candidates));
+    }
+
+
+    // 结果集合
+    private static final List<List<Integer>> result = new ArrayList<>();
+
+    private static int[] candidateValue;
+
+    public static List<List<Integer>> permute(int[] nums) {
+        candidateValue = nums;
+        backtrack(new ArrayList<>(), 0);
+
+        return result;
+
+
+    }
+
+    private static void backtrack(List<Integer> path, int start) {
+        // 结束条件：长度相等
+        if (path.size() == candidateValue.length) {
+            result.add(new ArrayList<>(path));
+            return;
+        }
+
+        for (int i = start; i < candidateValue.length; i++) {
+            int currentValue = candidateValue[i];
+            // 排除已存在的元素
+            if (path.contains(currentValue)) {
+                continue;
+            }
+
+            // 选择
+            path.add(currentValue);
+            // 回溯，考虑所有候选项
+            backtrack(path, 0);
+            // 撤销选择
+            path.remove(path.size() - 1);
+        }
+    }
+
+
+}
