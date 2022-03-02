7eb4abdcf29eea9fc914db3a245a08ec9786f818
Guanchen Zhao <guanchenzhao@gmail.com>
2022-03-01 21:53:10 -0800

    Create InvertBinaryTree.java

---
+++ src/leetcode/solution/Tree/InvertBinaryTree.java
@@ -0,0 +1,38 @@
+package leetcode.solution.Tree;
+
+import leetcode.Structure.TreeNode;
+
+/**
+ * 226. Invert Binary Tree
+ */
+public class InvertBinaryTree {
+
+    public static void main(String[] args) {
+        Integer[] pArray = {1, 2, 3, 4, 5};
+        TreeNode p = TreeNode.constructTree(pArray);
+        System.out.println(invertTree(p));
+
+    }
+
+    public static TreeNode invertTree(TreeNode root) {
+        helper(root);
+        return root;
+    }
+
+    private static void helper(TreeNode root) {
+        // 到达叶子节点，返回
+        if (root == null) {
+            return;
+        }
+
+        // 交换左右节点
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+        // 递归子节点
+        helper(root.left);
+        helper(root.right);
+
+    }
+}
