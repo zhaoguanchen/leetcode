f1d43f14836debf3ad6f3d3e4e24cd4b6961e41c
Guanchen Zhao <guanchenzhao@gmail.com>
2022-03-29 23:48:02 -0700

    数独
    回溯算法，判断合法性

--- src/leetcode/solution/BackTracking/GenerateParentheses.java
+++ src/leetcode/solution/BackTracking/GenerateParentheses.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 /**
  * 22. Generate Parentheses
- * Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
  */
 public class GenerateParentheses {
 
---
+++ src/leetcode/solution/BackTracking/SudokuSolver.java
@@ -0,0 +1,91 @@
+package leetcode.solution.BackTracking;
+
+import java.util.Arrays;
+
+/**
+ * 37. Sudoku Solver
+ */
+public class SudokuSolver {
+
+    public static void main(String[] args) {
+        char[][] board = {{'5', '3', '.', '.', '7', '.', '.', '.', '.'}, {'6', '.', '.', '1', '9', '5', '.', '.', '.'}, {'.', '9', '8', '.', '.', '.', '.', '6', '.'}, {'8', '.', '.', '.', '6', '.', '.', '.', '3'}, {'4', '.', '.', '8', '.', '3', '.', '.', '1'}, {'7', '.', '.', '.', '2', '.', '.', '.', '6'}, {'.', '6', '.', '.', '.', '.', '2', '8', '.'}, {'.', '.', '.', '4', '1', '9', '.', '.', '5'}, {'.', '.', '.', '.', '8', '.', '.', '7', '9'}};
+        SudokuSolver sudokuSolver = new SudokuSolver();
+        sudokuSolver.solveSudoku(board);
+        System.out.println(Arrays.deepToString(board));
+    }
+
+    private char[][] data;
+    private int length;
+
+    public void solveSudoku(char[][] board) {
+        data = board;
+        length = board.length;
+        backtrack(0, 0);
+
+    }
+
+
+    private boolean backtrack(int i, int j) {
+        // 到达最后一行
+        if (i == length) {
+            return true;
+        }
+        // 到达最右边，继续找下一行
+        if (j == length) {
+            return backtrack(i + 1, 0);
+        }
+        // 已经有数字
+        if (data[i][j] != '.') {
+            return backtrack(i, j + 1);
+        }
+
+
+        for (char k = '1'; k <= '9'; k++) {
+            // 做选择
+            if (!isOk(i, j, k)) {
+                continue;
+            }
+            data[i][j] = k;
+            boolean ans = backtrack(i, j + 1);
+            if (ans) {
+                return true;
+            }
+            // 撤销选择
+            data[i][j] = '.';
+        }
+
+        return false;
+    }
+
+
+    /**
+     * 判断在(i,j)位置选择字符k是否正确
+     *
+     * @param i
+     * @param j
+     * @param k
+     * @return
+     */
+    private boolean isOk(int i, int j, char k) {
+        for (int l = 0; l < length; l++) {
+            // 判断行
+            if (data[i][l] == k) {
+                return false;
+            }
+
+            // 判断列
+            if (data[l][j] == k) {
+                return false;
+            }
+
+            // 判断九宫格
+            int row = (i / 3) * 3 + l / 3;
+            int col = (j / 3) * 3 + l % 3;
+            if (k == data[row][col]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
