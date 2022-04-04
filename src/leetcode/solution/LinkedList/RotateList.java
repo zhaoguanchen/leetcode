54f5cde3c9433210d92af306a4242225518ffe43
Guanchen Zhao <guanchenzhao@gmail.com>
2022-04-03 22:34:54 -0700

    Create RotateList.java
    部分翻转链表  链表操作

---
+++ src/leetcode/solution/LinkedList/RotateList.java
@@ -0,0 +1,55 @@
+package leetcode.solution.LinkedList;
+
+import leetcode.Structure.ListNode;
+
+/**
+ * 61. Rotate List
+ */
+public class RotateList {
+
+    public static void main(String[] args) {
+        Integer[] array = {0, 1, 2};
+        ListNode l1 = ListNode.constructList(array);
+        int k = 4;
+        RotateList rotateList = new RotateList();
+        ListNode ans = rotateList.rotateRight(l1, k);
+        ListNode.print(ans);
+    }
+
+    public ListNode rotateRight(ListNode head, int k) {
+        if (head == null) {
+            return head;
+        }
+        ListNode cur = head;
+        int length = 0;
+        while (cur != null) {
+            cur = cur.next;
+            length++;
+        }
+
+        k = k % length;
+        // 无需处理
+        if (k == 0) {
+            return head;
+        }
+        // 找到要断开的前一节点
+        cur = head;
+        for (int i = 0; i < length - k - 1; i++) {
+            cur = cur.next;
+        }
+
+        // 新节点指针
+        ListNode vHead = cur.next;
+        // 设为null
+        cur.next = null;
+
+        // 寻找尾节点，链接到头节点
+        cur = vHead;
+        while (cur.next != null) {
+            cur = cur.next;
+        }
+
+        cur.next = head;
+        return vHead;
+    }
+}
