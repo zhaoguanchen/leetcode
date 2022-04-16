a0923a7d9a2c39989d60fb27c4a0991dfd4a8abf
Guanchen Zhao <guanchenzhao@gmail.com>
2022-04-16 15:51:17 -0700

    Update PartitionList.java

--- src/leetcode/solution/LinkedList/PartitionList.java
+++ src/leetcode/solution/LinkedList/PartitionList.java
@@ -17,16 +17,20 @@ public class PartitionList {
     }
 
     public ListNode partition(ListNode head, int x) {
-
+        // 虚拟节点
         ListNode vHead = new ListNode(0);
         vHead.next = head;
+        // 慢指针，指向最新确定的小于x的节点
         ListNode p1 = vHead;
+        // 快指针，寻找未知区间内小于x的节点；由于要操作节点，p2指向目标节点的前一节点（通过p2.next判断）
         ListNode p2;
 
+        // 如果链表前部节点小于x,则p1后移；
         while (p1.next != null && p1.next.val < x) {
             p1 = p1.next;
         }
 
+        // p1.next为下一个大于等于x的数，
         p2 = p1.next;
 
         while (p2 != null && p2.next != null) {
