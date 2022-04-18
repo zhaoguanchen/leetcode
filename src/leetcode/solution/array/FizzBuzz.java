fc49db8ba3c62a7c0faca79e4cc1fcce8ccf9879
Guanchen Zhao <guanchenzhao@gmail.com>
2022-04-17 23:46:39 -0700

    Create FizzBuzz.java

---
+++ src/leetcode/solution/array/FizzBuzz.java
@@ -0,0 +1,40 @@
+package leetcode.solution.array;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 412. Fizz Buzz
+ */
+public class FizzBuzz {
+
+    public static void main(String[] args) {
+        int n = 3;
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        List<String> ans = fizzBuzz.fizzBuzz(n);
+        System.out.println(ans);
+    }
+
+    public List<String> fizzBuzz(int n) {
+        List<String> ans = new ArrayList<>();
+
+        for (int i = 1; i <= n; i++) {
+            boolean divisibleBy3 = (i % 3 == 0);
+            boolean divisibleBy5 = (i % 5 == 0);
+
+            if (divisibleBy3 && divisibleBy5) {
+                ans.add("FizzBuzz");
+            } else if (divisibleBy3) {
+                ans.add("Fizz");
+
+            } else if (divisibleBy5) {
+                ans.add("Buzz");
+            } else {
+                ans.add(String.valueOf(i));
+            }
+
+        }
+
+        return ans;
+    }
+}
