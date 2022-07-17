f39bf68a345786e1795e22a3af3b47a374f78246
Guanchen Zhao <guanchenzhao@gmail.com>
2022-07-16 23:47:44 -0700

    Update CanPlaceFlowers.java

--- src/leetcode/solution/array/CanPlaceFlowers.java
+++ src/leetcode/solution/array/CanPlaceFlowers.java
@@ -2,10 +2,6 @@ package leetcode.solution.array;
 
 /**
  * 605. Can Place Flowers
- * <p>
- * You have a long flowerbed in which some of the plots are planted, and some are not. However, flowers cannot be planted in adjacent plots.
- * <p>
- * Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means not empty, and an integer n, return if n new flowers can be planted in the flowerbed without violating the no-adjacent-flowers rule.
  */
 public class CanPlaceFlowers {
 
